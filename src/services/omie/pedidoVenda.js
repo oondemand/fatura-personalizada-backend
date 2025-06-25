ff96e02f7fcb764d3a06f5abcd212f89b9a435ee
Maike <maikonalexandre574@gmail.com>
2025-06-25 16:33:32 -0300

    feat: enhance logging for order modification with structured JSON output

--- src/services/omie/pedidoVenda.js
+++ src/services/omie/pedidoVenda.js
@@ -54,7 +54,7 @@ exports.PedidoVendaOmie = {
         param: [pedido],
       };
 
-      console.log("--- Alterando pedido:", body);
+      console.log("--- Alterando pedido:", JSON.stringify(body, null, 2));
 
       const response = await apiOmie.post("produtos/pedido/", body);
       return response.data;
@@ -65,6 +65,11 @@ exports.PedidoVendaOmie = {
   },
 
   montarPedidoVendaAlterado: async ({ pedido, etapa, observacao }) => {
+    console.log(
+      "--- Montando pedido alterado",
+      JSON.stringify(pedido, null, 2)
+    );
+
     const pedidoAlterado = {
       Cabecalho: {
         codigo_pedido: pedido.pedido_venda_produto?.cabecalho?.codigo_pedido,
